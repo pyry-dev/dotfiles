0157ca7c1cc66e1c7627c0e81c938d21c8af9729
Pyry Kovanen <4008031+pyryjook@users.noreply.github.com>
2024-12-27 16:44:16 +0100

    Add auto-session plugin and its config

--- .config/nvim/lua/auto_session_config.lua
+++ .config/nvim/lua/auto_session_config.lua
@@ -1,5 +1,11 @@
-require('auto-session').setup {
-    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- Uncomment the following line to enable debug logging
-    -- log_level = 'debug',
-}
+local ok, auto_session = pcall(require, "auto-session")
+
+if ok then
+    auto_session.setup {
+        suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+        -- Uncomment the following line to enable debug logging
+        -- log_level = 'debug',
+    }
+else
+    vim.notify("auto-session plugin not found. Skipping configuration.", vim.log.levels.WARN)
+end
